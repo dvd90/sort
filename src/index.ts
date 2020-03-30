0295856069f1f81c04f611cc052772f8f131f0a4
David Sellam <dvdsellam@gmail.com>
2020-03-30 12:43:19 +0300

    buble sort with Typescript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('hqawrs');
+
+const sorter = new Sorter(numbersCollection);
+const sorter2 = new Sorter(charactersCollection);
+sorter.sort();
+sorter2.sort();
+
+console.log(charactersCollection);
